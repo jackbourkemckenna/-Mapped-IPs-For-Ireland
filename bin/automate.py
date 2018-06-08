b20ecd7e43f9fb00251f7094d13d16927b21ed2d
root <root@ip-172-31-21-179.eu-west-1.compute.internal>
2018-06-08 13:00:22 +0000

    test

--- bin/automate.py
+++ bin/automate.py
@@ -1,16 +1,12 @@
-
-
-
-from git import Repo,remote
-
-rw_dir = '/var/www/html/python/shit'
-repo = Repo(rw_dir)
-
-'''Enter code to commit the repository here.
-After commit run the following code to push the commit to remote repo.
-I am pushing to master branch here'''
-
-origin = repo.remote(name='origin')
-origin.push()
-
-
+import git
+repo = git.Repo( '/var/www/html/python/shit')
+print repo.git.status()
+# checkout and track a remote branch
+print repo.git.checkout( 'master' )
+# add a file
+print repo.git.add( '/var/www/html/python/shit/bin/test2.py' )
+# commit
+print repo.git.commit( m='my commit message' )
+# now we are one commit ahead
+print repo.git.status()
+# now push
