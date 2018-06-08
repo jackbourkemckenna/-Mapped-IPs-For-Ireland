990e77f98d5be5cdd24e364ad6f502236eda17e2
root <root@ip-172-31-21-179.eu-west-1.compute.internal>
2018-06-08 13:42:33 +0000

    removing files

--- bin/automate.py
+++ bin/automate.py
@@ -4,9 +4,12 @@ print repo.git.status()
 # checkout and track a remote branch
 print repo.git.checkout( 'master' )
 # add a file
-print repo.git.add( '/var/www/html/python/shit/bin/test2.py' )
+print repo.git.add( '/var/www/html/python/shit/bin/finaltest.py' )
 # commit
 print repo.git.commit( m='my commit message' )
+
+
+repo.git.push()
 # now we are one commit ahead
 print repo.git.status()
 # now push
